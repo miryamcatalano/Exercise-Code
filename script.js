c3751d62fcafc62361a64d56afbeb498ef0a9273
Miryam <miryam.catalano99@gmail.com>
2023-12-05 14:35:36 +0100

    new button count

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-document.getElementById('click').addEventListener('click', function(){
-    console.log("Bottone cliccato");
+var incrementButton = document.getElementById('click1');
+var decrementButton = document.getElementById('click2');
+
+var valore = 0;
+
+document.getElementById("click1").addEventListener("click", function(){
+    valore++;
+    document.getElementById('x').textContent = valore;
 });
+
+document.getElementById('click2').addEventListener('click', function(){
+
+    if(valore>0){
+    valore--;
+    document.getElementById('x').textContent = valore;
+    }
+
+    else{
+        alert("Impossibile decrementare");
+    }
+});
+
